5cc31b29f81bacf3e1a05d2db908b5a55d82aa5c
Corinna Pan <corinna.pan@gmail.com>
2020-05-14 16:29:10 -0400

    Updates

---
+++ precache-manifest.4bdd00c6295a118d33ecdad8517460b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb66005bb323037a187b8e27358a047b",
+    "url": "/corinnapan.github.io/index.html"
+  },
+  {
+    "revision": "c984843f668a7aa0b409",
+    "url": "/corinnapan.github.io/static/css/main.eafd669b.chunk.css"
+  },
+  {
+    "revision": "13a0e7b5d3eb55fc6422",
+    "url": "/corinnapan.github.io/static/js/2.b8159f8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corinnapan.github.io/static/js/2.b8159f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c984843f668a7aa0b409",
+    "url": "/corinnapan.github.io/static/js/main.07e1566c.chunk.js"
+  },
+  {
+    "revision": "9e2aab4dbd780d30deb2",
+    "url": "/corinnapan.github.io/static/js/runtime-main.b5371038.js"
+  }
+]);
