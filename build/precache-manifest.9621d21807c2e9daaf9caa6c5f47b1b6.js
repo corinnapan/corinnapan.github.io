35593828f82a687b8180af05fa04b4127b23911e
Corinna Pan <corinna.pan@gmail.com>
2020-05-14 17:58:37 -0400

    temp remove /build from gitignore so we can serve from it

---
+++ build/precache-manifest.9621d21807c2e9daaf9caa6c5f47b1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d95c5ef59e2f17b7e8da7f7ad94230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c984843f668a7aa0b409",
+    "url": "/static/css/main.eafd669b.chunk.css"
+  },
+  {
+    "revision": "13a0e7b5d3eb55fc6422",
+    "url": "/static/js/2.b8159f8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8159f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c984843f668a7aa0b409",
+    "url": "/static/js/main.07e1566c.chunk.js"
+  },
+  {
+    "revision": "82e71da4bc5fbf905687",
+    "url": "/static/js/runtime-main.27fe88e9.js"
+  }
+]);
